e3992f32644eebb884cb28f3cfe8abb9458e986a
Rod Andrade <randrade@brbatel.com.br>
2023-06-08 22:55:29 -0300

    feat import fonts web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className="text-2xl font-bold">oi</div>
+  return (
+    <>
+      <div className="text-2xl font-bold">oi</div>
+      <div className="font-alt text-2xl font-bold">oi</div>
+    </>
+  )
 }
